2f689a97d32503fce137dababacda1b7554268c1
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-04-12 11:58:26 +0400

    lesson 1

--- E-Commerce.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace E_Commerce.DataAccess
-{
-    public class Class1
-    {
-    }
-}
---
+++ E-Commerce.DataAccess/Concrete/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce.DataAccess.Concrete
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer()
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
